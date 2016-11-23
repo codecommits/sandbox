292f979bb0f0e3b43d7e10fda9153b0ed6186719
Madeny Diawara <madeny@live.com>
2016-11-24 00:18:57 +0100

    Auto updates

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php require 'framework/functions.php';
+$conn = connect($config);
+if (!$conn) {
+//   echo 'Problem to connect to database';
+} else {
+//   echo 'Everything is fine baby!';
+}
+// $posts = get('php', $conn);
+$posts = query('SELECT * FROM php WHERE id = :id', 
+				array('id' => $_GET['id']), $conn);
+// print_r($posts);
+
+
+$view_path = 'views/index.view.php';
+require 'views/layout.php';
--- views/index.view.php
+++ views/index.view.php
@@ -1,8 +1,12 @@
 <h1 class="h1"> Welcome to daily php code practice</h1>
   <hr>
 <?php foreach ($posts as $post) : ?>
-    <article>  
-      <h2 class="title"><?= $post['title']; ?></h2>
+     <article>  
+      <h2 class="title">
+     <a href="single.php?id= <?= $post['id']; ?>"> <?= $post['title']; ?></a>
+        
+
+      </h2>
     <div class="body">
     <?= $post['body']; ?>
     </div>
