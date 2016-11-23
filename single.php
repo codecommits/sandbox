4cd50554c9eca28aa5c549c7982535fac87c5358
Madeny Diawara <madeny@live.com>
2016-11-24 00:28:58 +0100

    Single post is working fine but have to follow the instruction!

--- single.php
+++ single.php
@@ -10,6 +10,7 @@ $posts = query('SELECT * FROM php WHERE id = :id',
 				array('id' => $_GET['id']), $conn);
 // print_r($posts);
 
-
+// this works fine but I have to followr the 
+// the instruction.
 $view_path = 'views/index.view.php';
 require 'views/layout.php';
